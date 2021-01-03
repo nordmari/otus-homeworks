4d4fa9fe0656b8f835a18643b1abc6ea2c30183e
borozdenko_m <borozdenko_m@utkonos.ru>
2021-01-03 12:57:41 +0300

    gradle homework 1

---
+++ hw01-gradle/src/main/java/ru/nordmari/hwgradle/HelloOtus.java
@@ -0,0 +1,10 @@
+package ru.nordmari.hwgradle;
+
+import com.google.common.collect.Lists;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        System.out.println(Lists.newArrayList("Hello Otus!"));
+    }
+}
