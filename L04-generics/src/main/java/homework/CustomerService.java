3e4128f7db55dafde1e743c073048ece48a6f5e7
borozdenko_m <borozdenko_m@utkonos.ru>
2021-01-25 10:13:44 +0300

    hw 2 generics

--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,35 @@
 package homework;
 
 
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+import static java.util.Comparator.comparingLong;
 
 public class CustomerService {
 
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final NavigableMap<Customer, String> customersMap = new TreeMap<>(comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> firstEntry = customersMap.firstEntry();
+        if (firstEntry == null) {
+            return null;
+        }
+        return new SimpleImmutableEntry<>(new Customer(firstEntry.getKey()), firstEntry.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> nextEntry = customersMap.higherEntry(customer);
+        if (nextEntry == null) {
+            return null;
+        }
+        return new SimpleImmutableEntry<>(new Customer(nextEntry.getKey()), nextEntry.getValue());
     }
 
     public void add(Customer customer, String data) {
-
+        customersMap.put(customer, data);
     }
 }
